b18815abfee877e79bb1cbcc1b403add2d23d243
Samuel Bryett <z5588327@ad.unsw.edu.au>
2025-01-24 16:26:37 +1100

    Initialise express app

---
+++ back-end/app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("Hello, world!"));
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`My first Express app - listening on port ${PORT}!`);
+});
