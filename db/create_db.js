0cdf959ed1857cf319b3692c590f9397ac742930
Samuel Bryett <z5588327@ad.unsw.edu.au>
2025-02-14 14:57:59 +1100

    Create script for creating users table into database

---
+++ db/create_db.js
@@ -0,0 +1,19 @@
+const { Client } = require('pg');
+require('dotenv').config();
+const SQL = `
+CREATE TABLE IF NOT EXISTS users (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  first VARCHAR(15),
+  last VARCHAR(15),
+  email VARCHAR(255),
+  password VARCHAR(25)
+  )
+`
+async function main() {
+  const client = new Client( {
+    connectionString: process.env.CONNECTION_STRING
+  });
+  await client.connect();
+  await client.query(SQL);
+  await client.end();
+}
